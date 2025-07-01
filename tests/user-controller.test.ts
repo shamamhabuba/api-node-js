9fa2ce6a0c2e72090dd995c4809fb57e63a0d1b6
mypc <shamamhabuba@gmail.com>
2025-07-01 19:49:53 +0300

    Fix/docker compose (#2)

--- tests/user-controller.test.ts
+++ tests/user-controller.test.ts
@@ -2,8 +2,7 @@
 import { test, expect } from '@playwright/test';
 let baseURL: string = 'http://localhost:3000/users';
 
-test.describe('User management API', () => {
-
+test.describe('User management API for empty users', () => {
     test('GET / - should return empty when no users', async ({ request }) => {
         const response = await request.get(`${baseURL}`);
         expect(response.status()).toBe(200);
@@ -11,24 +10,54 @@ test.describe('User management API', () => {
         expect(responseBody).toBe('[]');
     });
 
-    test('GET /:id - should return a user by ID', async ({ request }) => {
+})
+test.describe('User management API for existing users', () => {
+    test.skip('GET /:id - should return a user by ID', async ({ request }) => {
+        const response= await request.get(`${baseURL}`+"/100");
+        expect(response.status()).toBe(200);
+        const responseBody = await response.json()
+        expect(responseBody.id).toBe(100);
 
     });
 
-    test('GET /:id - should return 404 if user not found', async ({ request }) => {
-
+    test.skip('GET /:id - should return 404 if user not found', async ({ request }) => {
+        const response= await request.get(`${baseURL}`+"/101");
+        expect(response.status()).toBe(404);
     });
 
     test('POST / - should add a new user', async ({ request }) => {
-
+        const response= await request.post(`${baseURL}`);
+        expect(response.status()).toBe(201);
+        const responseBody = await response.json()
+        expect(responseBody.id).toBeDefined();
     });
 
     test('DELETE /:id - should delete a user by ID', async ({ request }) => {
+        //creat new user
+        const responseUserCreation= await request.post(`${baseURL}`);
+        expect(responseUserCreation.status()).toBe(201);
+        const responseBodyUserCreation = await responseUserCreation.json()
+        expect(responseBodyUserCreation.id).toBeDefined();
+        const userId= responseBodyUserCreation.id
+        //deletion of user
+        const response= await request.delete(`${baseURL}`+ "/"+userId);
+        expect(response.status()).toBe(200);
 
     });
 
     test('DELETE /:id - should return 404 if user not found', async ({ request }) => {
-
+//create user
+        const responseUserCreation= await request.post(`${baseURL}`);
+        expect(responseUserCreation.status()).toBe(201);
+        const responseBodyUserCreation = await responseUserCreation.json()
+        expect(responseBodyUserCreation.id).toBeDefined();
+        const userId= responseBodyUserCreation.id
+        //delete user by Id
+        const responseDeletionOne= await request.delete(`${baseURL}`+ "/"+userId);
+        expect(responseDeletionOne.status()).toBe(200);
+        // delete user again with the same Id
+        const responseDeletionTwo= await request.delete(`${baseURL}`+ "/"+userId);
+        expect(responseDeletionTwo.status()).toBe(404);
     });
 
 
